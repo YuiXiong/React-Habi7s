78266bc80d1961926bb8f6355b1ab55dd9df1e73
Yui Xiong Ng <jadebear15@gmail.com>
2022-10-29 18:50:30 +0800

    feat: show hide form on click

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,12 +1,19 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import CreateBoard from "./CreateBoard";
 
 function SideBar() {
   const [show, setShow] = useState(false);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const [isShown, setIsShown] = useState(false);
+  const handleClick = event => {
+    // 👇️ toggle shown state for create form
+    setIsShown(current => !current);
+// setIsShown(true);
+  };
 
   return (
     <>
@@ -17,9 +24,13 @@ function SideBar() {
       <Offcanvas show={show} onHide={handleClose}>
         <Offcanvas.Header closeButton>
           <Offcanvas.Title>Habi7s</Offcanvas.Title>
-          <Button>create new board</Button>
-
+          <Button onClick={handleClick}>create new board</Button>
+          {/* render create board component form when clicked */}
+          
         </Offcanvas.Header>
+        {isShown && (
+        <CreateBoard/>
+        )}
         <Offcanvas.Body>
          here is where i shall render all the buttons to the boards
         </Offcanvas.Body>
