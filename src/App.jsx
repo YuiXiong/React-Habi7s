1eda9683231947c58d2172049c3198b3c45a6eb1
Yui Xiong Ng <jadebear15@gmail.com>
2022-10-29 22:37:34 +0800

    feat: frame for todo container

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import Col from 'react-bootstrap/Col';
 
-import SideBar from './components/SideBar/SideBar'
+
+import SideBar from "./components/SideBar/SideBar";
+import TodoContainer from "./components/TodoContainer/TodoContainer";
 
 function App() {
   return (
     <div className="App">
-      <SideBar/>
+      <Container>
+        <Row>
+        <Col xs={2}><SideBar /></Col>
+        <Col><TodoContainer /></Col>
+        </Row>
+      </Container>
     </div>
   );
 }
