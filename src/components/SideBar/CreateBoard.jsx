3bf0660b89db3da414eb5622ec7355a20962df42
Yui Xiong Ng <jadebear15@gmail.com>
2022-10-29 18:57:35 +0800

    feat: styling and clean up form

--- src/components/SideBar/CreateBoard.jsx
+++ src/components/SideBar/CreateBoard.jsx
@@ -6,23 +6,16 @@ import "./CreateBoard.css"
 function CreateBoard() {
   return (
     <Form className="board">
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
+      <Form.Group className="mb-3" controlId="Board Name">
+        <Form.Label>Board Name:</Form.Label>
+        <Form.Control type="boardName" placeholder="Enter Board Name" />
       </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
+      
+      <Button className= "board-button" variant="primary" type="submit">
         Submit
+      </Button> 
+      <Button className= "board-button" variant="danger" type="cancel">
+        Cancel
       </Button>
     </Form>
   );
